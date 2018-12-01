5486f96dd5c6c1f754ef7a0e81d1e1b56cd05565
Uttam sharma <uttamsharma7361@gmail.com>
2018-12-01 14:56:36 +0530

    done

--- index.php
+++ index.php
@@ -1,3 +1,263 @@
-<?php
 
+<?php
+require_once('db.php');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2 class="text-center">Crud operation with AJAX</h2>
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#myModal">
+    Open modal
+  </button>
+
+  <!-- Button to Show Records -->
+  <button type="button" class="btn btn-light" onclick="show_data()">
+    Display
+  </button>
+  <h2 class="text-center">All Student Records</h2>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+      	<th>S No.</th>
+        <th>Name</th>
+        <th>Father Name</th>
+        <th>Email</th>
+        <th>Mobile Number</th>
+        <th>Degree</th>
+        <th>Year</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody id="records">
+      
+    </tbody>
+  </table>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Insert Data</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <form class="form-group" id="insertdata">
+          	<label class="col-form-label">Name:</label> 
+          	<input type="text" name="stu_name" id="stu_name" class="input-group">
+          	<label class="col-form-label">Father Name:</label> 
+          	<input type="text" name="f_name" id="f_name" class="input-group">
+          	<label class="col-form-label">Email:</label> 
+          	<input type="email" name="email" id="email" class="input-group">
+          	<label class="col-form-label">Mobile Number:</label> 
+          	<input type="number" name="mobile_number" id="mobile_number" class="input-group">
+          	<label class="col-form-label">Degree:</label> 
+          	<input type="text" name="degree" id="degree" class="input-group">
+          	<label class="col-form-label">Year:</label> 
+          	<input type="text" name="year" id="year" class="input-group">
+          </form>
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="insert_data()">Save</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  <div class="modal" id="editModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Edit Data</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          <form class="form-group" id="insertdata">
+          	<label class="col-form-label">Name:</label> 
+          	<input type="text" name="edit_stu_name" id="edit_stu_name" class="input-group">
+          	<label class="col-form-label">Father Name:</label> 
+          	<input type="text" name="edit_f_name" id="edit_f_name" class="input-group">
+          	<label class="col-form-label">Email:</label> 
+          	<input type="email" name="edit_email" id="edit_email" class="input-group">
+          	<label class="col-form-label">Mobile Number:</label> 
+          	<input type="number" name="edit_mobile_number" id="edit_mobile_number" class="input-group">
+          	<label class="col-form-label">Degree:</label> 
+          	<input type="text" name="edit_degree" id="edit_degree" class="input-group">
+          	<label class="col-form-label">Year:</label> 
+          	<input type="text" name="edit_year" id="edit_year" class="input-group">
+          	<input type="hidden" name="hidden_id" id="hidden_id">
+          </form>
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="update_data()">Update</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function(){
+		show_records();
+	});
+	function show_records() {
+		var records  = 'show';
+		$.ajax({
+			url		: 'operation.php',
+			type	: 'POST',
+			data 	: 
+			{
+				
+				records 		: records,
+
+			},
+			success: function(data, status){
+				if('success' == status) {
+					$("#records").html(data);
+				}
+			}
+		});
+	}
+	function insert_data() {
+		//alert($('#stu_name').val());
+		var stu_name = $('#stu_name').val();
+		var f_name = $('#f_name').val();
+		var email = $('#email').val();
+		var mobile_number = $('#mobile_number').val();
+		var degree = $('#degree').val();
+		var year = $('#year').val();
+
+		$.ajax({
+			url		: 'operation.php',
+			type	: 'POST',
+			data 	: 
+			{
+				
+				stu_name 		: stu_name,
+				f_name			: f_name,
+				email 			: email,
+				mobile_number 	: mobile_number,
+				degree 			: degree,
+				year 			: year,
+
+			},
+			success: function(data, status){
+				if('success' == status) {
+					show_records();
+				}
+				else{
+					alert("ERROR!!!!");
+				}
+			}
+		});
+		//alert("here");
+	}
+
+	function delete_data(id) {
+
+		var flag = confirm("Are You Sure");
+		if(flag){
+			$.ajax({
+				url		: 'operation.php',
+				type	: 'POST',
+				data 	: 
+				{
+					
+					id 		: id,
+
+				},
+				success: function(data, status){
+					if('success' == status) {
+						show_records();
+					}
+				}
+			});
+		}
+	}
+
+	function update_data() {
+		//alert($('#stu_name').val());
+		var edit_stu_name = $('#edit_stu_name').val();
+		var edit_f_name = $('#edit_f_name').val();
+		var edit_email = $('#edit_email').val();
+		var edit_mobile_number = $('#edit_mobile_number').val();
+		var edit_degree = $('#edit_degree').val();
+		var edit_year = $('#edit_year').val();
+
+		var hidden_id = $('#hidden_id').val();
+
+		$.ajax({
+			url		: 'operation.php',
+			type	: 'POST',
+			data 	: 
+			{
+				
+				edit_stu_name 		: edit_stu_name,
+				edit_f_name			: edit_f_name,
+				edit_email 			: edit_email,
+				edit_mobile_number 	: edit_mobile_number,
+				edit_degree 			: edit_degree,
+				edit_year 			: edit_year,
+				hidden_id			: hidden_id,
+
+			},
+			success: function(data, status){
+				if('success' == status) {
+					$('#editModal').modal('hide');
+					show_records();
+				}
+				else{
+					alert("ERROR!!!!");
+				}
+			}
+		});
+		//alert("here");
+	}
+
+	function edit_data(id) {
+		
+		$('#hidden_id').val(id);
+
+		$.post('operation.php',
+				{edit_id:id},
+				function(data, status) {
+					var user = JSON.parse(data);
+					$('#edit_stu_name').val(user.s_name);
+					$('#edit_f_name').val(user.s_f_name);
+					$('#edit_email').val(user.s_email);
+					$('#edit_mobile_number').val(user.s_m_number);
+					$('#edit_degree').val(user.s_degree);
+					$('#edit_year').val(user.s_year);
+				}
+		);
+		$('#editModal').modal('show');
+	}
+</script>
+</body>
+</html>
---
+++ operation.php
@@ -0,0 +1,69 @@
+<?php 
+require_once('db.php');
+
+extract($_POST);
+
+if(isset($_POST['stu_name']) && isset($_POST['f_name']) && isset($_POST['email']) && isset($_POST['mobile_number']) && isset($_POST['degree']) && isset($_POST['year'])) {
+	$q  = "INSERT INTO `student` (`s_name`, `s_f_name`, `s_email`, `s_m_number`, `s_degree`, `s_year`) values('$stu_name', '$f_name', '$email', '$mobile_number', '$degree', '$year') ";
+
+	mysqli_query($con, $q);
+}
+
+if(isset($_POST['records'])) {
+	$q  = "SELECT * FROM `student`";
+
+	$result = mysqli_query($con, $q);
+
+	if( mysqli_num_rows($result) > 0 ) {
+		$number = 1;
+		$data = "";
+		while ($row = mysqli_fetch_array($result)) {
+			$data .= '<tr>
+						<td>'.$number.'</td>
+						<td>'.$row["s_name"].'</td>
+						<td>'.$row["s_f_name"].'</td>
+						<td>'.$row["s_email"].'</td>
+						<td>'.$row["s_m_number"].'</td>
+						<td>'.$row["s_degree"].'</td>
+						<td>'.$row["s_year"].'</td>
+						<td><button type="button" class="btn btn-primary d-flex justify-content-center btn-warning" onclick="edit_data('.$row["s_id"].')">Edit</button></td>
+						<td><button type="button" class="btn btn-primary justify-content-center btn-danger" onclick="delete_data('.$row["s_id"].')">Delete</button></td>
+					</tr>';
+			$number++;
+		}
+	}
+	echo $data;
+}
+
+if(isset($_POST['id']) && '' != $_POST['id'] ) {
+
+	$q  = "DELETE FROM `student` WHERE s_id = $id";
+
+	$result = mysqli_query($con, $q);
+}
+
+if(isset($_POST['edit_id']) && '' != $_POST['edit_id'] ) {
+	$user_id = $_POST['edit_id'];
+	$que  = "SELECT * FROM student WHERE s_id = '$user_id'";
+
+	$results = mysqli_query($con, $que);
+	$response = array();
+	if( mysqli_num_rows($results) > 0 ) {
+		while ($row = mysqli_fetch_assoc($results)) {
+			$response = $row;
+		}
+	}
+	else{
+		$response['status'] = 200;
+	}
+
+	echo json_encode($response);
+}
+
+if(isset($_POST['edit_stu_name']) && isset($_POST['edit_f_name']) && isset($_POST['edit_email']) && isset($_POST['edit_mobile_number']) && isset($_POST['edit_degree']) && isset($_POST['edit_year']) && isset($_POST['hidden_id'])) {
+	var_dump($edit_mobile_number);
+	$q  = "UPDATE `student` SET `s_name` = '$edit_stu_name', `s_f_name` = '$edit_f_name', `s_email` = '$edit_email', `s_m_number` = '$edit_mobile_number', `s_degree` = '$edit_degree', `s_year` = '$edit_year' WHERE s_id = '$hidden_id' ";
+
+	mysqli_query($con, $q);
+}
+?>
